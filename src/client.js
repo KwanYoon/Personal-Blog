973d10abd2935cce2421308047c52d71eadcadfe
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2021-01-09 14:55:16 -0500

    first commit

---
+++ src/client.js
@@ -0,0 +1,9 @@
+// communication to sanity.io
+
+// Basic imports
+import sanityClient from '@sanity/client';
+
+export default sanityClient({
+    projectId: "2h13hnwp",
+    dataset: "personalsite"
+})
